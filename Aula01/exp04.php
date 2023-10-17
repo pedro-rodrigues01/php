a45fe817464d7d99df86948fd545434a62b62577
pedro-rodrigues01 <145489129+pedro-rodrigues01@users.noreply.github.com>
2023-10-17 16:20:18 -0300

    Add files via upload

---
+++ Aula01/exp04.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$txt = "Hello Word!";
+$x = 5; 
+$y = 10.5;
+echo "$txt <br>";
+echo "O valor de x + y é <p>". "$x + $y =". $x + $y;
+?>
+</body>
+</html>
---
+++ Aula01/exp06.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Sschools.com";
+$x = 5;
+$y = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "Stury  PHP at" . $txt2 . "<br>";
+print $x + $y;
+
+
+?>
+<?php
+print "<h2>PHP is Fun! </h2>";
+print "Hello Word";
+print " I am about to learn PHP!";
+?>
+
+</body>
+</html>
