ef0ea6d0bb75788e2d1b29bda9e2b19ed9a6aae3
pedro-rodrigues01 <145489129+pedro-rodrigues01@users.noreply.github.com>
2023-10-18 17:18:49 -0300

    aula2

---
+++ aula02/exp01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp01</title>
+</head>
+<link rel="stylesheet" href="exp01.css">
+<body>
+
+    <pre> seletor </pre>
+    <h1> titulo 1 </h1>
+    <h2> subtitulo </h2>
+    <p> paragrafo </p>
+
+    
+    <pre> seletores com uso de id= para especificar cada elemento </pre>
+    <p id= "stilo1"> paragrafo </p>
+    <p> paragrafo </p>
+    <p> paragrafo </p>
+    <p class="stilo2" >  paragrafo </p>
+    <p class="stilo2" > paragrafo </p>
+    <p class="stilo2" > usa-se pra varios elementos</p>
+
+    
+</body>
+</html>
---
+++ aula02/exp02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+    <h1> Titulo </h1>
+    <p class="estilo"> Classe 01</p>
+    
+    <p> p fora  </p>
+
+    <div class="conteudo">
+    
+    <p> titulo da div </p>
+    
+    <p id= "titulo"> subconteudo </p>
+    
+</div>
+    
+
+
+</body>
+</html>
